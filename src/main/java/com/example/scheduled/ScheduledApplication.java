1bfb3a9a56a555165aa884c11cf336785857f78b
Lucas Lima <lluuccaassfm@gmail.com>
2021-11-16 23:49:34 -0300

    Add Scheduled Task

--- src/main/java/com/example/scheduled/ScheduledApplication.java
+++ src/main/java/com/example/scheduled/ScheduledApplication.java
@@ -2,8 +2,10 @@ package com.example.scheduled;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScheduledApplication {
 
     public static void main(String[] args) {
